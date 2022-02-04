e3fcbfdb8649a1f2306b16969a1ffee9aaa42880
suyogsubedi <subedisuyog555@gmail.com>
2022-02-04 11:46:37 +0545

    Day 1

---
+++ Day 1/app.js
@@ -0,0 +1,25 @@
+// 3 ways
+// var,let,const
+// console.log(usingVar);
+// console.log(usingLet);
+var usingVar = 15;
+let usingLet = 18;
+
+// console.log(usingLet);
+// usingLet = 20;
+// console.log(usingLet);
+// Constants
+
+// this variable stores constant
+const myConst = "Suyog";
+
+// Not allowed
+// myConst = "Princess";
+
+let environment = "Polluted";
+console.log(environment);
+console.log(environment.toUpperCase());
+
+console.log(Math.random());
+
+console.log(myConst);
